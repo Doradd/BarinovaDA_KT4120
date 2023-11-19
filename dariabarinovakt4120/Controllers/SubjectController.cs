50e51126377aedb320192f265d9fc895aa5068d9
Даша <@DESKTOP-2T1UDC3>
2023-11-19 22:41:52 +0300

    add service

---
+++ dariabarinovakt4120/Controllers/SubjectController.cs
@@ -0,0 +1,29 @@
+﻿using dariabarinovakt4120.Filters;
+using dariabarinovakt4120.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dariabarinovakt4120.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SubjectController : Controller
+    {
+        private readonly ILogger<SubjectController> _logger;
+        private readonly ISubjectService _subjectService;
+
+        public SubjectController(ILogger<SubjectController> logger, ISubjectService subjectService)
+        {
+            _logger = logger;
+            _subjectService = subjectService;
+        }
+
+        [HttpPost(Name = "GetSubjectBySpec")]
+
+        public async Task<IActionResult> GetSubjectBySpecAsync(SubjectSpecFilter filter, CancellationToken cancellationToken = default)
+        {
+            var subject = await _subjectService.GetSubjectBySpecAsync(filter, cancellationToken);
+            return Ok(subject);
+        }
+    }
+
+}
---
+++ dariabarinovakt4120/Filters/SubjectSpecFilter.cs
@@ -0,0 +1,9 @@
+﻿namespace dariabarinovakt4120.Filters
+{
+    public class SubjectSpecFilter
+    {
+        public string? Directionname { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
+
